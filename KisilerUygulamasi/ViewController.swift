5822eedcc59ab1b261d04a3baa0e5fabafb40749
aliosmandurmaz <odurmaz.34@outlook.com>
2022-03-21 23:16:29 +0300

    Initial Commit

---
+++ KisilerUygulamasi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KisilerUygulamasi
+//
+//  Created by Ali Osman DURMAZ on 21.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
